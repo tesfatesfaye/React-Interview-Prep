39e13db4043a0f819c99328bb5aa745f62c7ba6c
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-07-29 16:04:46 -0400

    started this project today

---
+++ Quizzical/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import StartingPage from './Pages/StartingPage'
+const App = () => {
+
+  return (
+    <div className="App">
+      
+   <StartingPage/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ Quizzical/src/Pages/StartingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const StartingPage=()=>{
+
+    return(
+        <div className="startingPage">
+            <h1>Quizzical</h1>
+            <h2>Challenge yourself to better yourself</h2>
+
+        </div>
+    )
+
+}
+
+export default StartingPage
