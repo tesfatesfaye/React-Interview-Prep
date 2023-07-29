d3fe230fbce09ee6a8d49bc751596fa206ded7e4
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-07-29 18:12:58 -0400

    implemented routes in react router for quiz and app pages

--- Quizzical/src/App.jsx
+++ Quizzical/src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css'
 import StartingPage from './Pages/StartingPage'
+import { Routes, Route } from 'react-router-dom';
 import Quiz from './Pages/Quiz'
+
 const App = () => {
 
   return (
     <div className="App">
-      
-   <StartingPage/>
+    <Routes>
+      <Route path='/' element={<StartingPage />}/>
+        <Route path='/quiz' element={<Quiz />}/>
+
+       
+
+    </Routes>
 
 
-   
       <div className='blue-back{'></div>
       <div className='yellow-back'></div>
 
