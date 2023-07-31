870a625615f2c9125afedb0329ed1b60f7d282d1
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-07-31 12:50:06 -0400

    created and used a fetcher util function

--- Quizzical/src/Context/QuizContext.jsx
+++ Quizzical/src/Context/QuizContext.jsx
@@ -1,6 +1,7 @@
-import { nanoid } from 'nanoid';
-import React, { createContext, useEffect, useLayoutEffect, useState } from "react";
+
+import React, { createContext, useLayoutEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import fetcher from '../../Util/fetcher';
 
 const QuizContext = createContext()
 
@@ -14,28 +15,7 @@ const QuizContextProvider = ({ children }) => {
     const [submitError, setSubmitError] = useState(false)
     const location = useLocation();
     useLayoutEffect(() => {
-        let info = []
-        const answerSetter = (val, correct) => {
-            return { id: nanoid(), val: val, correct: correct, selected: false }
-        }
-        const fetcher = async () => {
-            let quizInfo = []
-            const res = await fetch("https://opentdb.com/api.php?amount=5&type=multiple")
-            const data = await res.json()
-            info = await data.results
-            for (let i of info) {
-                quizInfo.push({ id: nanoid(), question: i.question, answers: [] })
-                let quizInfoLength = quizInfo.length - 1
-                for (let j of i.incorrect_answers) {
-                    quizInfo[quizInfoLength].answers.push(answerSetter(j, false))
-                }
-                let placer = Math.random() * i.incorrect_answers.length
-                quizInfo[quizInfoLength].answers.splice(placer, 0, answerSetter(i.correct_answer, true))
-            }
-            setQuizData(quizInfo)
-
-        }
-        fetcher()
+        fetcher(setQuizData)
     }, [startGame])
     const currentPage = location.pathname
     const selectAnswerChoice = (id, parentId) => {
